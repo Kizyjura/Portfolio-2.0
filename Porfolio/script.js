573b10ca1fb8f653e8a8ad15141416234506e018
Radley <101191748+Kizyjura@users.noreply.github.com>
2025-06-28 18:26:28 +0200

    Delete Porfolio/script.js

--- Porfolio/script.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('explore-btn').addEventListener('click', () => {
-  document.getElementById('skills').scrollIntoView({ behavior: 'smooth' });
-});
-
-// Animate Hero Text
-window.addEventListener('load', () => {
-  document.getElementById('hero-text').style.opacity = 1;
-});
